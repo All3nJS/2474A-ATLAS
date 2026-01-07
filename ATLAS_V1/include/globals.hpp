dc00f6f30be54a54811b3f5546e6aea73a5afef0
xoomify <allensumesh@gmail.com>
2026-01-06 21:35:23 -0600

    Revert "testing global pnuematic"
    This reverts commit 3593c5bdf2b14b002add683909b2c9b0b98c7331.

--- ATLAS_V1/include/globals.hpp
+++ ATLAS_V1/include/globals.hpp
@@ -1,7 +1,3 @@
 #include "main.h"
 extern pros::Motor A7;
 extern pros::Motor A8;
-extern pros::adi::Pneumatics descore;
-extern pros::adi::Pneumatics redirect;
-extern pros::adi::Pneumatics scraper;
-extern pros::adi::Pneumatics double_park;  
--- ATLAS_V1/include/helpers.hpp
+++ ATLAS_V1/include/helpers.hpp
@@ -2,7 +2,3 @@
 
 void stage1(int power1);
 void stage2(int power2);
-void scraper();
-void redirect();
-void double_park();
-void descore();
