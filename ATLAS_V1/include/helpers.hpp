3593c5bdf2b14b002add683909b2c9b0b98c7331
xoomify <allensumesh@gmail.com>
2026-01-06 20:49:36 -0600

    testing global pnuematic

--- ATLAS_V1/include/helpers.hpp
+++ ATLAS_V1/include/helpers.hpp
@@ -2,3 +2,7 @@
 
 void stage1(int power1);
 void stage2(int power2);
+void scraper();
+void redirect();
+void double_park();
+void descore();
--- ATLAS_V1/src/globals.cpp
+++ ATLAS_V1/src/globals.cpp
@@ -2,3 +2,9 @@
 
 pros::Motor A7(15, pros::v5::MotorGears::blue);
 pros::Motor A8(16, pros::v5::MotorGears::blue);
+
+
+pros::adi::Pneumatics descore({17, 'a'}, false);   
+pros::adi::Pneumatics double_park({17, 'b'}, false);  
+pros::adi::Pneumatics scraper({17, 'c'}, false);  
+pros::adi::Pneumatics redirect({17, 'd'}, false);  
